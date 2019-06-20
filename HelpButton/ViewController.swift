44bb33c0e625011b158b03260caebfaaf83807b0
Nurmerey Shakhanova <shakhanova.n@gmail.com>
2019-06-19 17:18:18 -0700

    BLE scanning and gist adding done

--- HelpButton/ViewController.swift
+++ HelpButton/ViewController.swift
@@ -8,20 +8,60 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CBCentralManagerDelegate {
+    var centralManager: CBCentralManager!
     let gistPath = "https://api.github.com/gists"
     var helpButtonGistId: String? = nil
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
 
     @IBOutlet weak var helpButtonReference: UIButton!
     @IBOutlet weak var deleteButtonReference: UIButton!
+    var bleDevice:CBPeripheral!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        centralManager = CBCentralManager(delegate: self, queue: nil)
+
         if(self.helpButtonGistId==nil){
             self.deleteButtonReference.isEnabled = false;
             self.deleteButtonReference.alpha = 0.3;
         }
+    }
+
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+
+        switch central.state {
+        case .unknown:
+            print("central.state is .unknown")
+        case .resetting:
+            print("central.state is .resetting")
+        case .unsupported:
+            print("central.state is .unsupported")
+        case .unauthorized:
+            print("central.state is .unauthorized")
+        case .poweredOff:
+            print("central.state is .poweredOff")
+        case .poweredOn:
+            print("central.state is .poweredOn")
+            centralManager.scanForPeripherals(withServices: nil)//[myBLEServiceCBUUID])
+        @unknown default:
+            print("fatal error!")
+        }
+    }
+
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral,
+                        advertisementData: [String: Any], rssi RSSI: NSNumber) {
+        if(peripheral.identifier.description=="375DFD02-7B00-D7BC-ACA3-9CCFC2D7D415"){ // feel free to plug in your favorite ble device UUID
+            centralManager.stopScan()
+            bleDevice = peripheral
+            centralManager.connect(bleDevice)
+        }
+    }
+
+    func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
+        print("Connected!", peripheral)
+        self.activityIndicator.startAnimating();
+        makeGistWithDeviceInfo(deviceInfo: peripheral.description)
     }
     
     @IBAction func helpButton(_ sender: UIButton) {
@@ -93,7 +133,6 @@ class ViewController: UIViewController {
         case server
     }
     func makePostAPICall() -> Result<String?, NetworkError> {
-
         guard let url = URL(string: gistPath) else {
             return .failure(.url)
         }
@@ -103,8 +142,6 @@ class ViewController: UIViewController {
         urlRequest.httpMethod = "POST"
         urlRequest.setValue("Basic \(base64LoginData)", forHTTPHeaderField: "Authorization")
         urlRequest.httpBody = Data("{\"description\":\"HelpButton API\",\"public\":\"true\",\"files\":{\"help.txt\":{\"content\":\"Button Press!\"}}".utf8)
-
-
         return runDataTask(urlRequest: urlRequest)
     }
 
@@ -119,6 +156,19 @@ class ViewController: UIViewController {
         urlRequest.setValue("Basic \(base64LoginData)", forHTTPHeaderField: "Authorization")
         return runDataTask(urlRequest: urlRequest)
 
+    }
+
+    func makeGistWithDeviceInfo(deviceInfo:String) {
+        let url = URL(string: gistPath)
+        let loginData = String(format: "%@:%@", username, token).data(using: String.Encoding.utf8)!
+        let base64LoginData = loginData.base64EncodedString()
+        var urlRequest = URLRequest(url: url!)
+        urlRequest.httpMethod = "POST"
+        urlRequest.setValue("Basic \(base64LoginData)", forHTTPHeaderField: "Authorization")
+        urlRequest.httpBody = Data("{\"description\":\"HelpButton API\",\"public\":\"true\",\"files\":{\"deviceInfo.txt\":{\"content\":\"\(deviceInfo))\"}}".utf8)
+        let result = runDataTask(urlRequest: urlRequest)
+        print("result", result)
+        self.activityIndicator.stopAnimating();
     }
 
     func runDataTask(urlRequest:URLRequest)->Result<String?, NetworkError>{
